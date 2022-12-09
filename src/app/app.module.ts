32e142f1aeb1c86121956ffe86176c9d09eaabb2
Maicon Araujo de Oliveira <maicon.ao@gmail.com>
2022-12-08 21:00:13 -0300

    Commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { Module1Module } from './module1/module1.module';
+import { Module2Module } from './module2/module2.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    Module1Module,
+    Module2Module
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/service2.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Service2 {
+
+  text = "Service 2";
+
+  constructor() {
+
+    console.log("Service 2 - Constructor");
+
+   }
+     
+}
